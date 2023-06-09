101962ec73ab555019107c28eb7d6756aa31ff00
araaiko <araaiko.122@gmail.com>
2023-06-09 10:20:28 +0900

    react-routerの設定とauthSliceの作成（Redux）

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+/** 外部import */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+/** 内部import */
+import styles from "./App.module.css";
+import { Auth } from "./components/Auth";
+import { MainPage } from "./components/MainPage";
+
+const App = () => {
   return (
-    <div className="App">
-      test
+    <div className={styles.app__root}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/vehicle" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
